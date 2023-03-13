0acbcf9e76ea6806b915c2705191d2c8d9645513
Domidomi1 <dominika.wen@gmail.com>
2023-03-13 16:56:07 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/task/CoordinateSystem.java
+++ src/main/java/pl/javastart/task/CoordinateSystem.java
@@ -6,6 +6,10 @@ public class CoordinateSystem {
 
     // uzupełnij metodę. Używaj scannera przekazanego w parametrze.
     void run(Scanner scanner) {
+        System.out.println("Punkt (0, 0)");
+        System.out.println("Punkt (0, -15)");
+        System.out.println("Punkt (15, 0)");
         System.out.println("Punkt (5, -3)");
+
     }
 }
